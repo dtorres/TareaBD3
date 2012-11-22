74d9cbb48374037eec1057d4134424420aec05c4
Diego Torres <warorface@gmail.com>
2012-11-22 00:48:02 -0300

    Base stuff

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+  <meta charset="utf-8">
+	<title>ELECCIONES CEEINF 2013</title>
+	<meta name="description" content="" />
+  	<meta name="keywords" content="" />
+	<meta name="robots" content="" />
+</head>
+<body>
+
+<h1>Bienvenido</h1>
+
+<form method="post" action="login.php">
+
+<label for="user">Usuario:</label>
+<input type="text" name="user"/>
+<label for="user">Password:</label>
+<input type="password" name="pass"/>
+
+<input type="submit" />
+
+</form>
+
+<a href="register.php">Registrate Aqui</a>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+  <meta charset="utf-8">
+	<title>Registrate</title>
+	<meta name="description" content="" />
+  	<meta name="keywords" content="" />
+	<meta name="robots" content="" />
+</head>
+<body>
+
+<h1>Registrate</h1>
+
+<form method="post">
+
+<label>Nombre</label>
+<input name="name"/>
+<label>E-Mail</label>
+<input name="mail"/>
+<label>Clave</label>
+<input name="pass" type="password"/>
+
+<input type="submit"/>
+
+</form>
+
+</body>
+</html>
